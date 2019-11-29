93b13c13bdc7905a372e6b5d0f12e7f2033653ba
andreas <andreas.vlachos@arhs-dev-hellas.com>
2019-11-29 15:11:00 +0200

    Renamed directories

---
+++ 1-first-app/src/test/java/gr/arhs/firtapp/FirtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.arhs.firtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
